f07f7a5d233c88730e14d20425cca013a56b31d4
CarolinnaCiocanu <92712560+CarolinnaCiocanu@users.noreply.github.com>
2022-12-26 13:31:37 +0200

    NgRx Store.
    * Added auth reducer
    * Merged reducers

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,44 +1,64 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
-import { Subscription } from "rxjs";
-import { AuthService } from "../auth/auth.service";
-import { DataStorageService } from "../shared/data-storage.service";
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
+import { AuthService } from '../auth/auth.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import * as fromApp from '../store/app.reducer';
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-    @Output() featureSelected = new EventEmitter<string>();
-    isAuthenticated: boolean = false;
-    private userSub: Subscription;
+  @Output() featureSelected = new EventEmitter<string>();
+  isAuthenticated: boolean = false;
+  private userSub: Subscription;
 
-    constructor(private dataStorageDevice: DataStorageService, private authService: AuthService) { }
+  constructor(
+    private dataStorageDevice: DataStorageService,
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
-    ngOnInit(): void {
-        this.userSub = this.authService.user.subscribe(user => {
-            this.isAuthenticated = !!user;
-            console.log(!user);
-            console.log(!!user);
-        });
-    }
+  ngOnInit(): void {
+    this.userSub = this.store
+      .select('auth')
+      .pipe(
+        map((authState) => {
+          return authState.user;
+        })
+      )
+      .subscribe((user) => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      });
+  }
 
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 
-    onSaveData() {
-        this.dataStorageDevice.storeRecipes();
-    }
+  onSaveData() {
+    this.dataStorageDevice.storeRecipes();
+  }
 
-    onLogout() {
-        this.authService.logout();
-    }
+  onLogout() {
+    this.authService.logout();
+  }
 
-    onFetchData() {
-        this.dataStorageDevice.fetchRecipes().subscribe();
-    }
+  onFetchData() {
+    this.dataStorageDevice.fetchRecipes().subscribe();
+  }
 
-    ngOnDestroy(): void {
-        this.userSub.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
